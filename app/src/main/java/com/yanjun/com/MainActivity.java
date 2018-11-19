3e33136356002118f2538bfa1a93bb59195947e8
yanjun.zhao <yanjun.zhao@cootek.cn>
2018-11-19 18:27:05 +0800

    commit

--- app/src/main/java/com/yanjun/com/MainActivity.java
+++ app/src/main/java/com/yanjun/com/MainActivity.java
@@ -2,12 +2,24 @@ package com.yanjun.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.image1);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mImageView.setSelected(!mImageView.isSelected());
+            }
+        });
     }
 }
